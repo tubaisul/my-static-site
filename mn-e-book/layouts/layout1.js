8158aea62cd576c55cedc8cd4b095552255c3913
baisul <kelietubayasula@gmail.com>
2024-10-03 15:37:37 +0900

    Describe your changes here

--- mn-e-book/layouts/layout1.js
+++ mn-e-book/layouts/layout1.js
@@ -1,7 +1,6 @@
 function renderLayout1(data) {
   const contentDiv = document.getElementById("content");
   contentDiv.innerHTML = `<h1>${data.title}</h1>`;
-
   data.sections.forEach((section) => {
     const sectionDiv = document.createElement("div");
     sectionDiv.className = "section";
@@ -10,13 +9,5 @@ function renderLayout1(data) {
       <div class="content">${section.content}</div>
     `;
     contentDiv.appendChild(sectionDiv);
-
-    // 检查当前小节是否需要展示 Jupyter Notebook
-    if (section.include_notebook) {
-      const jupyterLiteDiv = document.createElement("div");
-      jupyterLiteDiv.style = "height: 600px; width: 100%; margin-top: 20px;";
-      sectionDiv.appendChild(jupyterLiteDiv);
-      renderJupyterLite(section.notebook, jupyterLiteDiv);
-    }
   });
 }
